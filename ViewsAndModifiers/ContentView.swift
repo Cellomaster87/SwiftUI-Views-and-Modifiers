2d8ce2a1a9eb13ff9c8f1a540c507b81949ddc16
Michele Galvagno <michicello@gmail.com>
2019-10-19 20:44:18 +0200

    Project 3 challenge 1

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -9,52 +9,17 @@
 import SwiftUI
 
 struct ContentView: View {
+//    var body: some View {
+//        GridStack(rows: 4, columns: 4) { (row, col) in
+//            HStack {
+//                Image(systemName: "\(row * 4 + col).circle")
+//                Text("R\(row) C\(col)")
+//            }
+//        }
+//    }
     var body: some View {
-//        Text("Hello World")
-//            .titleStyle()
-        Color.blue
-            .frame(width: 300, height: 200)
-            .watermarked(with: "Hacking with Swift")
-    }
-}
-
-/// Custom modifier for TITLE
-struct Title: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .foregroundColor(.white)
-            .padding()
-            .background(Color.blue)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-    }
-}
-
-extension View {
-    func titleStyle() -> some View {
-        self.modifier(Title())
-    }
-}
-
-/// Custom view structure
-struct Watermark: ViewModifier {
-    var text: String
-    
-    func body(content: Content) -> some View {
-        ZStack(alignment: .bottomTrailing) {
-            content
-            Text(text)
-                .font(.caption)
-                .foregroundColor(.white)
-                .padding(5)
-                .background(Color.black)
-        }
-    }
-}
-
-extension View {
-    func watermarked(with text: String) -> some View {
-        self.modifier(Watermark(text: text))
+        Text("STAR WARS")
+            .prominentTitle()
     }
 }
 
